72e4a7a8f891a55ee443eab18282c7c3f539d183
Rexhep <rexhepberlajolli@gmail.com>
2018-09-16 23:55:41 +0200

    Initial project setup

---
+++ Insurance/settings/dev.py
@@ -0,0 +1,51 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5n$pp1rl5xqvf7=!4ikzfob9b=)j%78df9bnkawz84wsl-z48z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insurance',
+        'USER': 'iwt',
+        'PASSWORD': '42d31ae0c55d494b8861be0e6',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s] [%(asctime)s] [%(module)s] %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'DEBUG'
+        }
+    }
+}
---
+++ Insurance/settings/prod.py
@@ -0,0 +1,24 @@
+from .base import *
+
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = [os.environ['SECRET_KEY']]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT']
+    },
+}
