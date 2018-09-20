e56b8aed601dde4b0b94516c9b3f69cc46710f52
Rexhep <rexhepberlajolli@gmail.com>
2018-09-20 21:19:59 +0200

    Add Risk Type and RiskField model

--- custom/models.py
+++ custom/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class RiskType(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class RiskField(models.Model):
+    TYPE_CHOICES = (
+        ('text', 'Text'),
+        ('enum', 'Enum'),
+        ('date', 'Date'),
+        ('number', 'Number'),
+        ('currency', 'Currency'),
+        ('option', 'Option'),
+        ('color', 'Color'),
+        ('bool', 'Boolean'),
+    )
+
+    name = models.CharField(max_length=120)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES,
+                            default='text')
+    options = ArrayField(models.CharField(max_length=50), default=list)
+    risk_type = models.ForeignKey(RiskType, related_name='risk_fields',
+                                  on_delete=models.CASCADE)
