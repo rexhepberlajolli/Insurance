c26d70e0e56e7007fb8fb08405c5c2a737ba168e
Rexhep <rexhepberlajolli@gmail.com>
2018-09-20 21:22:02 +0200

    Include custom urls in base one and add obtaion auth token view

--- Insurance/urls.py
+++ Insurance/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', ObtainAuthToken.as_view(), name='authenticate'),
+    path('api/v1/custom/', include('custom.urls', namespace='Custom'))
 ]
