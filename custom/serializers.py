31eaa761699fef51c1878e3526febf6f1e1af9e7
Rexhep <rexhepberlajolli@gmail.com>
2018-09-20 21:20:38 +0200

    Add risk type and risk field serializers

---
+++ custom/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class RiskFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RiskField
+        fields = ('id', 'name', 'type', 'options')
+        read_only_fields = ('id',)
+        extra_kwargs = {'options': {'required': False}}
+
+
+class RiskTypeListSerializer(serializers.ModelSerializer):
+    risk_fields = RiskFieldSerializer(many=True)
+
+    class Meta:
+        model = models.RiskType
+        fields = ('id', 'name', 'risk_fields')
+        read_only_fields = ('id', )
+
+    def create(self, validated_data):
+        risk_fields = validated_data.pop('risk_fields')
+        risk_type = super(RiskTypeListSerializer, self).create(validated_data)
+        for field in risk_fields:
+            risk_type.risk_fields.create(**field)
+        return risk_type
