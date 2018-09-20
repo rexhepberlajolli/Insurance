8d1fdda3b59b51699bc15125575ae34ffa39c73b
Rexhep <rexhepberlajolli@gmail.com>
2018-09-20 21:22:12 +0200

    Add rest pagination settings

--- Insurance/settings/base.py
+++ Insurance/settings/base.py
@@ -105,8 +105,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
-        'rest_framework.parsers.FileUploadParser',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
