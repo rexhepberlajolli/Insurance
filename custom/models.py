59f0f9082749802f8dc10e15416c121d6e52f90b
Rexhep <rexhepberlajolli@gmail.com>
2018-09-21 22:07:52 +0200

    Add ordering, and verbose names to models

--- custom/models.py
+++ custom/models.py
@@ -5,6 +5,14 @@ from django.contrib.postgres.fields import ArrayField
 class RiskType(models.Model):
     name = models.CharField(max_length=120)
 
+    class Meta:
+        ordering = ('-id', )
+        verbose_name = 'Risk Type'
+        verbose_name_plural = 'Risk Types'
+
+    def __str__(self):
+        return self.name
+
 
 class RiskField(models.Model):
     TYPE_CHOICES = (
@@ -24,3 +32,11 @@ class RiskField(models.Model):
     options = ArrayField(models.CharField(max_length=50), default=list)
     risk_type = models.ForeignKey(RiskType, related_name='risk_fields',
                                   on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('id', )
+        verbose_name = 'Risk Field'
+        verbose_name_plural = 'Risk Fields'
+
+    def __str__(self):
+        return f"{self.name} {self.type}"
