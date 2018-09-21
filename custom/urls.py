07410203f166722f005aaabb556729794fb5e3cd
Rexhep <rexhepberlajolli@gmail.com>
2018-09-21 22:54:21 +0200

    Add retrieve update destroy view for risk types

--- custom/urls.py
+++ custom/urls.py
@@ -7,4 +7,7 @@ app_name = 'Custom'
 urlpatterns = [
     path('riskTypes/', views.ListCreateRiskTypeView.as_view(),
          name='list-create-risk-type'),
+    path('riskTypes/<int:pk>/',
+         views.RetrieveUpdateDestroyRiskTypeView.as_view(),
+         name='detail-risk-type'),
 ]
--- custom/views.py
+++ custom/views.py
@@ -7,3 +7,11 @@ class ListCreateRiskTypeView(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAdminUserOrReadOnly, )
     serializer_class = serializers.RiskTypeListSerializer
     queryset = models.RiskType.objects.all()
+
+
+class RetrieveUpdateDestroyRiskTypeView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAdminUserOrReadOnly, )
+    serializer_class = serializers.RiskTypeListSerializer
+    queryset = models.RiskType.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'pk'
