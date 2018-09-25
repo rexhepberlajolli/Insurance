21bace7be7c44116f86f5be0305c3e8d96921a0e
Rexhep <rexhepberlajolli@gmail.com>
2018-09-26 01:00:10 +0200

    Fix styles

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactSVG from 'react-svg';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router-dom';
 import { Navbar, Container, NavbarToggler, Collapse, Nav, NavItem } from 'reactstrap';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { makeSelectLocation } from '../../containers/App/selectors';
 
 import Logo from '../../images/logo.svg';
 
 import './styles/main.scss';
 
+const insurancePaths = [
+  {
+    name: 'Risk Types',
+    path: '/',
+  },
+];
+
 class Header extends Component {
+  static propTypes = {
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+
   state = {
     isOpen: false,
   };
@@ -17,8 +36,18 @@ class Header extends Component {
     this.setState(({ isOpen }) => ({ isOpen: !isOpen }));
   }
 
+  itemClass(p) {
+    const { path } = p;
+    const { location } = this.props;
+    const { pathname } = location;
+    const active = path === pathname ? 'active' : '';
+    return ['navbar-item', active].join(' ');
+  }
+
   render() {
+    const { location } = this.props;
     const { isOpen } = this.state;
+    const { pathname } = location;
 
     return (
       <header>
@@ -32,9 +61,13 @@ class Header extends Component {
             </NavbarToggler>
             <Collapse isOpen={isOpen} navbar>
               <Nav className="ml-auto" navbar>
-                <NavItem className="navbar-item">
-                  <Link className="navbar-link" to="/">Risk Types</Link>
-                </NavItem>
+                {
+                  insurancePaths.map((path) => (
+                    <NavItem className={this.itemClass(path)} key={path.name}>
+                      <Link className="navbar-link" to="/">Risk Types</Link>
+                    </NavItem>
+                  ))
+                }
               </Nav>
             </Collapse>
           </Navbar>
@@ -44,4 +77,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+  location: makeSelectLocation(),
+});
+
+const withConnect = connect(mapStateToProps, null);
+
+export default compose(withConnect)(Header);
--- frontend/containers/App/selectors.js
+++ frontend/containers/App/selectors.js
@@ -2,14 +2,18 @@
  * The global state selectors
  */
 
-// import { createSelector } from 'reselect';
+import { createSelector,  } from 'reselect';
 
 const selectGlobal = (state) => state.get('global');
 
 const selectRoute = (state) => state.get('route');
 
+const makeSelectLocation = () => createSelector(selectRoute,
+  (subState) => subState.get('location').toJS()
+);
+
 
 export {
   selectGlobal,
-  selectRoute,
+  makeSelectLocation,
 };
