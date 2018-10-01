0b4e033c7d61c2390824d0f5e1c7a109cad5e71f
Rexhep <rexhepberlajolli@gmail.com>
2018-10-02 00:36:50 +0200

    Add ListCreateRiskResult view

--- custom/views.py
+++ custom/views.py
@@ -1,6 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, views
 
-from . import serializers, models, permissions
+from . import serializers, models, permissions, mixins
 
 
 class ListCreateRiskTypeView(generics.ListCreateAPIView):
@@ -15,3 +15,18 @@ class RetrieveUpdateDestroyRiskTypeView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.RiskType.objects.all()
     lookup_field = 'id'
     lookup_url_kwarg = 'pk'
+
+
+class ListCreateRiskResultsView(mixins.RiskResultListMixin,
+                                mixins.RiskResultCreateMixin,
+                                generics.GenericAPIView):
+    """
+    Concrete view for listing risk type results or creating one
+    """
+    risk_type_model = models.RiskType
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
